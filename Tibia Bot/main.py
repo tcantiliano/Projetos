5c9381f48fd8801c0d6bdc43f864d6302673f6a3
Izarkon <tcantiliano86@gmail.com>
2025-03-20 21:45:19 -0300

    atualizando codigo

--- Tibia Bot/main.py
+++ Tibia Bot/main.py
@@ -1,1 +1,14 @@
-print("teste up")
+import pyautogui as pg
+import time 
+
+time.sleep(1)
+
+
+def pressKey():
+    pg.PAUSE = 2
+    pg.press('num1')
+    pg.press('num2')
+    print("done")
+    
+pressKey()
+
